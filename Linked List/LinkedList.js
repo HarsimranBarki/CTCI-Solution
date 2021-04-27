bcc6da933d561cc15fb5e5cee2640a30bc19b384
HarsimranBarki <harsimransinghbarki@gmail.com>
2021-04-27 18:49:05 +0530

    added find the kth element

--- Linked List/LinkedList.js
+++ Linked List/LinkedList.js
@@ -61,8 +61,57 @@ class LinkedList {
     }
   }
 
+  //   Remove Duplicate
+
+  removeDuplicate() {
+    if (!this.head || !this.tail) return null;
+    if (this.head == this.tail) return null;
+
+    let pointerOne = this.head;
+
+    while (pointerOne.next != null) {
+      let pointerTwo = pointerOne;
+
+      while (pointerTwo.next != null) {
+        if (pointerOne.data == pointerTwo.next.data) {
+          pointerTwo.next = pointerTwo.next.next;
+        }
+
+        pointerTwo = pointerTwo.next;
+      }
+
+      pointerOne = pointerOne.next;
+    }
+  }
+
+  kthToLast(pos) {
+    //assuming we don't have a size
+    let size = 0;
+    let curr = this.head;
+    while (curr.next != null) {
+      size++;
+      curr = curr.next;
+    }
+
+    if (pos > size) return console.log("Position Doesn't Exist");
+
+    let elePos = size - pos;
+    let counterTwo = 0;
+
+    let currTwo = this.head;
+    while (currTwo.next != null) {
+      if (counterTwo == elePos) return console.log(currTwo.data);
+      counterTwo++;
+      currTwo = currTwo.next;
+    }
+  }
+
   show() {
-    return console.log(this.head);
+    let curr = this.head;
+    while (curr.next != null) {
+      console.log(curr.data);
+      curr = curr.next;
+    }
   }
 }
 
@@ -73,9 +122,16 @@ class Node {
   }
 }
 
-let Book = new LinkedList();
+let UnsortedNums = new LinkedList();
 
-Book.append("Harry Potter");
-Book.append("Red Card");
-Book.append("Galaxy Guide");
-Book.show();
+UnsortedNums.append(5);
+UnsortedNums.append(8);
+UnsortedNums.append(3);
+UnsortedNums.append(1);
+UnsortedNums.append(9);
+UnsortedNums.append(3);
+UnsortedNums.append(2);
+UnsortedNums.append(6);
+UnsortedNums.append(7);
+UnsortedNums.kthToLast(2);
+UnsortedNums.show();
